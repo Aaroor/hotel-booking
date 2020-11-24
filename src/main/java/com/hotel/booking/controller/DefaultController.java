065510e6b47ddbd3cbf6130da89d85bacc22f88b
Aarooran kanthasamy <aaroor121819@gmail.com>
2020-11-24 21:50:31 +0530

    added

--- src/main/java/com/hotel/booking/controller/DefaultController.java
+++ src/main/java/com/hotel/booking/controller/DefaultController.java
@@ -7,36 +7,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DefaultController {
 	@GetMapping("/")
 	public String home1() {
-		return "/home";
+		return "home";
 	}
 
 	@GetMapping("/home")
 	public String home() {
-		return "/home";
+		return "home";
 	}
 
 	@GetMapping("/admin")
 	public String admin() {
-		return "/admin";
+		return "admin";
 	}
 
 	@GetMapping("/user")
 	public String user() {
-		return "/user";
+		return "user";
 	}
 
 	@GetMapping("/about")
 	public String about() {
-		return "/about";
+		return "about";
 	}
 
 	@GetMapping("/login")
 	public String login() {
-		return "/login";
+		return "login";
 	}
 
 	@GetMapping("/403")
 	public String error403() {
-		return "/403";
+		return "403";
 	}
 }
