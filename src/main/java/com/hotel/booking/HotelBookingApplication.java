05c16f8a10196696b0967e38258e740d59574daa
Aarooran kanthasamy <aaroor121819@gmail.com>
2020-11-26 12:24:49 +0530

    added

--- src/main/java/com/hotel/booking/HotelBookingApplication.java
+++ src/main/java/com/hotel/booking/HotelBookingApplication.java
@@ -2,6 +2,9 @@ package com.hotel.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class HotelBookingApplication {
@@ -9,5 +12,6 @@ public class HotelBookingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelBookingApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/hotel/booking/config/SpringSecurityConfig.java
+++ src/main/java/com/hotel/booking/config/SpringSecurityConfig.java
@@ -4,49 +4,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.AccessDeniedHandler;
-
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AccessDeniedHandler accessDeniedHandler;
+
 	// roles admin allow to access /admin/**
-    // roles user allow to access /user/**
-    // custom 403 access denied handler
+	// roles user allow to access /user/**
+	// custom 403 access denied handler
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-        .authorizeRequests()
-            .antMatchers("/","/login*","/home", "/about","/css/**","/webjars/**","/403").permitAll()
-            .antMatchers("/admin/**").hasAnyRole("ADMIN")
-            .antMatchers("/user/**").hasAnyRole("USER")
-            .anyRequest().authenticated()
-        .and()
-        .requiresChannel().anyRequest().requiresSecure()
-        .and()
-        .portMapper().http(80).mapsTo(443)
-        .and()
-        .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-        .logout()
-            .permitAll()
-            .and()
-        .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+		http.authorizeRequests()
+				.antMatchers("/", "/login", "/home", "/about", "/css/**", "/webjars/**", "/403").permitAll()
+				.antMatchers("/admin/**").hasAnyRole("ADMIN").antMatchers("/user/**").hasAnyRole("USER").anyRequest()
+				.authenticated().and()
+				// .requiresChannel().anyRequest().requiresSecure()
+				.formLogin().loginPage("/login").permitAll().and().logout().permitAll().and().exceptionHandling()
+				.accessDeniedHandler(accessDeniedHandler)
+				.and()
+                .csrf().disable()
+                .requiresChannel()		//config request to use the mapping to a required channel
+					.anyRequest().requiresSecure();
 		
-		//http.requiresChannel().anyRequest().requiresSecure();
-	}
-	
-	// create two users, admin and user
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		http
+        .portMapper()
+			.http(80).mapsTo(443);
 
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("ADMIN");
-    }
+		// http.requiresChannel().anyRequest().requiresSecure();
+	}
+
+	// create two users, admin and user
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER").and().withUser("admin")
+				.password("{noop}password").roles("ADMIN");
+	}
 
 }
